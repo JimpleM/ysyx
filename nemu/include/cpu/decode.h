25ee560a59887d5eeb938d118c869baa4f568a80
tracer-ysyx <tracer@ysyx.org>
2023-09-25 13:19:59 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:19:59 up  4:22,  4 users,  load average: 1.06, 1.18, 1.09

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -92,7 +92,6 @@ finish:
   pattern_decode(pattern, STRLEN(pattern), &key, &mask, &shift); \
   if ((((uint64_t)INSTPAT_INST(s) >> shift) & mask) == key) { \
     INSTPAT_MATCH(s, ##__VA_ARGS__); \
-    isa_reg_display(); \
     goto *(__instpat_end); \
   } \
 } while (0)
