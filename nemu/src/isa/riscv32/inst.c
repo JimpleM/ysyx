e0d40ebcc1c9ebd956de4a326e271f282590d1ca
tracer-ysyx <tracer@ysyx.org>
2023-09-25 12:32:08 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:32:08 up  3:34,  4 users,  load average: 0.91, 0.94, 0.99

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -41,7 +41,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   *rd     = BITS(i, 11, 7);
   
   switch (type) {
-    case TYPE_I: src1R();          immI(); Log("abasfadas"); break;
+    case TYPE_I: src1R();          immI();break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
   }
