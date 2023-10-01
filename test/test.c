94e7e1481ca18334f7435cd5e425a2c0e529ad86
tracer-ysyx <tracer@ysyx.org>
2023-10-01 13:26:02 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:02 up  3:23,  2 users,  load average: 0.01, 0.18, 0.19

--- test/test.c
+++ test/test.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <assert.h>
 char str1[] = "Hello";
-char str[20];
+char str[100];
 char * number_to_str(char *str, int number, int scale){
   if(number < scale){
     *str++ = number + '0';
@@ -20,24 +20,23 @@ int sprintf_t(char *out, const char *fmt, ...) {
   char *fmt_t = (char *)fmt;
   char *out_t = (char *)out;
 
-  int count = 0;
   char* ArgStr = NULL;  // 接收字符型
   int ArgInt = 0;  // 接收整型
   va_list args;
   va_start(args,fmt);
 
   while(*fmt_t != '\0'){
-    if(*fmt == ' '){
+    // printf("%s\n",str);
+    if(*fmt_t == ' '){
       *out_t++ = ' ';
-    }else if(*fmt == '='){
+    }else if(*fmt_t == '='){
       *out_t++ = '=';
-    }else if(*fmt == '+'){
+    }else if(*fmt_t == '+'){
       *out_t++ = '+';
-    }else if(*fmt == '\n'){
+    }else if(*fmt_t == '\n'){
       *out_t++ = '\n';
     }
-    
-    else if(*fmt == '%'){
+    else if(*fmt_t == '%'){
       fmt_t++;
       if(*fmt_t == 's'){
         ArgStr = va_arg(args, char*);
@@ -52,16 +51,17 @@ int sprintf_t(char *out, const char *fmt, ...) {
         out_t = number_to_str(out_t,ArgInt,10);
       }
     }
-
+    printf("%s\n",fmt_t);
     fmt_t++;
+
   }
   *out_t++ = '\0';
   va_end(args);
   return strlen(out);
 }
 int main(){
-	sprintf_t(str, "%s +", "Hello world!\n");
-  printf("%d\n",strcmp(str, "Hello world!\n +") == 0);
+	sprintf_t(str, "%s 123", "Hello world!");
+  printf("%d\n",strcmp(str, "Hello world! +") == 0);
   printf("%s\n",str);
   sprintf_t(str, "%d", 100);
   printf("%d\n",strcmp(str, "100") == 0);
