14e97609d5be691d4f8706894d9b644a822e4f2c
tracer-ysyx <tracer@ysyx.org>
2023-10-01 12:50:56 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:50:56 up  2:48,  2 users,  load average: 0.04, 0.14, 0.18

--- test/test.c
+++ test/test.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 // #include <string.h>
-void *memset(void *s, int c, size_t n) {
-  char *str = (char *)s;
-//   assert(*str != '\0');
-  for(;n;n--){
-    *str++ = c;
-  }
-  return str;
-}
-
 char str1[] = "Hello";
 char str[20];
+char * number_to_str(char *str, int number, int scale){
+  if(number < scale){
+    *str = number + '0';
+    return str;
+  }else{
+    char *str_t = number_to_str(str,number/scale,scale);
+    *str_t++ = number%scale + '0';
+    return str_t;
+  }
+}
+
+void number_to_str2(char **s, int number, int scale){
+  if(number < scale){
+    **s++ = number + '0';
+  }else{
+    number_to_str2(s,number/scale,scale);
+    **s++ = number%scale + '0';
+  }
+}
 int main(){
-	strcat(strcpy(str, str1), s[3]);
-	printf("%s\n",s3);
-	printf("%s\n",s3+2);
+  // number_to_str(str,12345,10);
+	number_to_str2(&str,12345,10);
+	printf("%s\n",str);
 	return 0;
 }
