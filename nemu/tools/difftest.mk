4a68e3aac9db81f1b1b9680f0319dd483b8cdbaa
tracer-ysyx <tracer@ysyx.org>
2023-10-02 15:49:53 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:53 up 16:16,  2 users,  load average: 0.21, 0.26, 0.38

--- nemu/tools/difftest.mk
+++ nemu/tools/difftest.mk
@@ -19,12 +19,9 @@ DIFF_REF_SO = $(DIFF_REF_PATH)/build/$(GUEST_ISA)-$(call remove_quote,$(CONFIG_D
 MKFLAGS = GUEST_ISA=$(GUEST_ISA) SHARE=1 ENGINE=interpreter
 ARGS_DIFF = --diff=$(DIFF_REF_SO)
 
-NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt 
-NEMUFLAGS += -b
-
 ifndef CONFIG_DIFFTEST_REF_NEMU
 $(DIFF_REF_SO):
-	$(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS) ARGS="$(NEMUFLAGS)"
+	$(MAKE) -s -C $(DIFF_REF_PATH) $(MKFLAGS)
 endif
 
 .PHONY: $(DIFF_REF_SO)
