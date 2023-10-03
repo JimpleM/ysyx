f4cb82a721ae927d7d5b45c2d153ed4d185274ea
tracer-ysyx <tracer@ysyx.org>
2023-10-03 16:21:08 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:08 up  6:02,  2 users,  load average: 0.25, 0.33, 0.33

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -27,9 +27,9 @@ void init_ftrace(const char *elf_file){
 		printf("%d\n",ehdr.e_phoff);
 	}
 
-	if(fread(&buffer, sizeof(Elf32_Shdr), ehdr.e_shnum, elf_fp) == 1){
+	// if(fread(&buffer, sizeof(Elf32_Shdr), ehdr.e_shnum, elf_fp) == 1){
 
-	}
+	// }
 
 	if(fread(&buffer, sizeof(Elf32_Shdr), ehdr.e_shnum, elf_fp) == ehdr.e_shnum){
 		// 查找符号表头并拷贝出来备用
