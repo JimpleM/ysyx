6ff351ca8bfa851a95d26eef25e41efc502817b5
tracer-ysyx <tracer@ysyx.org>
2023-10-03 16:34:00 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:00 up  6:15,  2 users,  load average: 0.95, 0.54, 0.38

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -4,7 +4,13 @@
 
 FILE *elf_fp = NULL;
 
-// static int ReadDataFromFile(FILE)
+static void ReadDataFromFile(void *buffer,uint32_t size, uint32_t n, uint32_t offset, FILE *elf_fp){
+	int ret = 0;
+	ret = fseek(elf_fp, offset, SEEK_SET);
+	assert(ret < 0);
+	ret = fread(buffer, size, n, elf_fp);
+	assert(ret != n);
+}
 
 
 void init_ftrace(const char *elf_file){
@@ -23,32 +29,31 @@ void init_ftrace(const char *elf_file){
 	// const Elf32_Phdr* pPhdr = (const Elf32_Phdr *) buffer;
 	const Elf32_Shdr* pShdr = (const Elf32_Shdr *) buffer;
 	// Elf32_Shdr shdr = {0};  
-	if(fread(&ehdr, sizeof(Elf32_Ehdr), 1, elf_fp) == 1){
-		printf("%d\n",ehdr.e_phoff);
+	
+	ReadDataFromFile(&ehdr,sizeof(Elf32_Ehdr), 1, 0, elf_fp);
+	printf("%d\n",ehdr.e_phoff);
+
+	if(fread(&buffer, sizeof(Elf32_Phdr), ehdr.e_phnum, elf_fp) == ehdr.e_phnum){
+
 	}
 
-	if(fread(&buffer, sizeof(Elf32_Phdr), ehdr.e_phnum, elf_fp) == 1){
 
-	}
-
-	if(fread(&buffer, sizeof(Elf32_Shdr), ehdr.e_shnum, elf_fp) == ehdr.e_shnum){
-		printf("adfsd\n");
-		// 查找符号表头并拷贝出来备用
-		for (int i = 0; i < ehdr.e_shnum; i++)
+	ReadDataFromFile(&buffer,sizeof(Elf32_Shdr), ehdr.e_shnum, ehdr.e_shoff, elf_fp);
+	// 查找符号表头并拷贝出来备用
+	for (int i = 0; i < ehdr.e_shnum; i++)
+	{
+		if (pShdr[i].sh_type == SHT_SYMTAB)
 		{
-			if (pShdr[i].sh_type == SHT_SYMTAB)
-			{
-				printf("%d\n",pShdr[i].sh_type);
-				memcpy(&ShdrSym, &pShdr[i], sizeof(Elf32_Shdr));
+			printf("%d\n",pShdr[i].sh_type);
+			memcpy(&ShdrSym, &pShdr[i], sizeof(Elf32_Shdr));
 
-				// 查找字符串表头并拷贝出来备用
-				if (pShdr[ShdrSym.sh_link].sh_type == SHT_STRTAB)
-				{
-					memcpy(&ShdrStr, &pShdr[ShdrSym.sh_link], sizeof(Elf32_Shdr));
-					// found = 1;
-					printf("%d\n",i);
-					break;
-				}
+			// 查找字符串表头并拷贝出来备用
+			if (pShdr[ShdrSym.sh_link].sh_type == SHT_STRTAB)
+			{
+				memcpy(&ShdrStr, &pShdr[ShdrSym.sh_link], sizeof(Elf32_Shdr));
+				// found = 1;
+				printf("%d\n",i);
+				break;
 			}
 		}
 	}
