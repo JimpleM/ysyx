49ddb411919d80d8e12d8b4654e1e0f25d1e841f
tracer-ysyx <tracer@ysyx.org>
2023-10-03 23:42:25 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:25 up 13:24,  2 users,  load average: 0.85, 0.78, 0.81

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -95,7 +95,8 @@ void init_ftrace(const char *elf_file){
 	// printf("%s\n",buffer);
 	
 }
-void print_space(int n){
+void print_space(uint32_t address,int  n){
+	printf("0x%8x:",address);
 	for(int i=0; i<n; i++){
 		printf("  ");
 	}
@@ -104,14 +105,14 @@ void print_space(int n){
 void ftrace_print(uint32_t pc, uint32_t npc,uint32_t inst){
 	for(int i=0; i<func_cnt; i++){
 		if(inst == 0x00008067 && (pc >= func_trace[i].symbol.st_value && pc <= func_trace[i].symbol.st_value + func_trace[i].symbol.st_size )){
-			print_space(output_cnt);
+			print_space(pc,output_cnt);
 			printf("ret  [%s]\n",func_trace[i].str);
 			output_cnt--;
 			return ;
 		}
 		if(npc == func_trace[i].symbol.st_value){
-			print_space(output_cnt);
-			printf("0x%8x: call[%s@0x%8x]\n",pc,func_trace[i].str,npc);
+			print_space(pc,output_cnt);
+			printf("call [%s@0x%8x]\n",func_trace[i].str,npc);
 			output_cnt++;
 			return ;
 		}
