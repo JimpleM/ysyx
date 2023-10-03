845609f733020d1abf38f0966b15457529937d0c
tracer-ysyx <tracer@ysyx.org>
2023-10-03 15:58:01 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:01 up  5:39,  2 users,  load average: 0.09, 0.32, 0.33

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -12,4 +12,12 @@ void init_ftrace(const char *elf_file){
 		elf_fp = fp;
 	}
 	Log("ELF is read from %s", elf_file ? elf_file : "stdout");
+
+	Elf32_Ehdr ehdr = {0};      // ELF文件信息头
+
+	if(fread(&ehdr, sizeof(Elf32_Ehdr), 1, elf_fp) == 1){
+		printf("%d\n",ehdr.e_phoff);
+	}
+
+	
 }
