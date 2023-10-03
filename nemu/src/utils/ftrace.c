2565640e490e6668e212f24f35b55f35f0e091c6
tracer-ysyx <tracer@ysyx.org>
2023-10-03 16:52:17 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:17 up  6:34,  2 users,  load average: 0.36, 0.26, 0.25

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -23,41 +23,32 @@ void init_ftrace(const char *elf_file){
 	Log("ELF is read from %s", elf_file ? elf_file : "stdout");
 
 	uint8_t buffer[1024] = {0};
-	Elf32_Ehdr ehdr = {0};      // ELF文件信息头
-	Elf32_Shdr ShdrSym = {0};   // 符号表头
-  	Elf32_Shdr ShdrStr = {0};   // 字符串表头
-	// const Elf32_Phdr* pPhdr = (const Elf32_Phdr *) buffer;
-	const Elf32_Shdr* pShdr = (const Elf32_Shdr *) buffer;
-	// Elf32_Shdr shdr = {0};  
+	Elf32_Ehdr elf_hdr = {0};      // ELF文件信息头
+	Elf32_Shdr sym_hdr = {0};   // 符号表头
+  	Elf32_Shdr str_hdr = {0};   // 字符串表头
 	
-	ReadDataFromFile(&ehdr,sizeof(Elf32_Ehdr), 1, 0, elf_fp);
-	printf("%d\n",ehdr.e_phoff);
+	// Read ELF Header
+	ReadDataFromFile(&elf_hdr,sizeof(Elf32_Ehdr), 1, 0, elf_fp);
 
-	if(fread(&buffer, sizeof(Elf32_Phdr), ehdr.e_phnum, elf_fp) == ehdr.e_phnum){
-
-	}
-
-
-	ReadDataFromFile(&buffer,sizeof(Elf32_Shdr), ehdr.e_shnum, ehdr.e_shoff, elf_fp);
-	// 查找符号表头并拷贝出来备用
-	for (int i = 0; i < ehdr.e_shnum; i++)
-	{
-		if (pShdr[i].sh_type == SHT_SYMTAB)
-		{
-			printf("%d\n",pShdr[i].sh_type);
-			memcpy(&ShdrSym, &pShdr[i], sizeof(Elf32_Shdr));
-
-			// 查找字符串表头并拷贝出来备用
-			if (pShdr[ShdrSym.sh_link].sh_type == SHT_STRTAB)
-			{
-				memcpy(&ShdrStr, &pShdr[ShdrSym.sh_link], sizeof(Elf32_Shdr));
-				// found = 1;
-				printf("%d\n",i);
-				break;
-			}
+	// Read Section Headers
+	ReadDataFromFile(&buffer,sizeof(Elf32_Shdr), elf_hdr.e_shnum, elf_hdr.e_shoff, elf_fp);
+	
+	const Elf32_Shdr* pShdr = (const Elf32_Shdr *) buffer;
+	for (int i = 0; i < elf_hdr.e_shnum; i++){
+		// Search for symbol tab
+		if (pShdr[i].sh_type == SHT_SYMTAB){
+			memcpy(&sym_hdr, &pShdr[i], sizeof(Elf32_Shdr));
+		}
+		// Search for string tab
+		if (pShdr[i].sh_type == SHT_STRTAB){
+			memcpy(&str_hdr, &pShdr[i], sizeof(Elf32_Shdr));
+			break;	// don't found shshtrtab
 		}
 	}
-	
-
+	// Read symtab
+	ReadDataFromFile(&buffer, sym_hdr.sh_size, 1, sym_hdr.sh_offset, elf_fp);
+	for(int i=0; i<sym_hdr.sh_size; i++){
+		printf("%c",buffer[i]);
+	}
 	
 }
