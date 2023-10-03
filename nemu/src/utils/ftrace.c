9f0456928f593d04b9f03e1634f7f2418a5df72e
tracer-ysyx <tracer@ysyx.org>
2023-10-03 16:15:32 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:15:32 up  5:57,  2 users,  load average: 0.62, 0.57, 0.40

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -16,17 +16,32 @@ void init_ftrace(const char *elf_file){
 	}
 	Log("ELF is read from %s", elf_file ? elf_file : "stdout");
 
-	// uint8_t buffer[1024] = {0};
+	uint8_t buffer[1024] = {0};
 	Elf32_Ehdr ehdr = {0};      // ELF文件信息头
+	Elf32_Shdr ShdrSym = {0};   // 符号表头
+  	Elf32_Shdr ShdrStr = {0};   // 字符串表头
 	// const Elf32_Phdr* pPhdr = (const Elf32_Phdr *) buffer;
-	// const Elf32_Shdr* pShdr = (const Elf32_Shdr *) buffer;
-	Elf32_Shdr shdr = {0};  
+	const Elf32_Shdr* pShdr = (const Elf32_Shdr *) buffer;
+	// Elf32_Shdr shdr = {0};  
 	if(fread(&ehdr, sizeof(Elf32_Ehdr), 1, elf_fp) == 1){
 		printf("%d\n",ehdr.e_phoff);
 	}
-	for(int i=0; i<ehdr.e_shnum;i++){
-		if(fread(&shdr, sizeof(Elf32_Shdr), 1, elf_fp) == 1){
-			printf("%d\n",shdr.sh_addr);
+	if(fread(&buffer, sizeof(Elf32_Shdr), ehdr.e_shnum, elf_fp) == 1){
+		// 查找符号表头并拷贝出来备用
+		for (int i = 0; i < ehdr.e_shnum; i++)
+		{
+			if (pShdr[i].sh_type == SHT_SYMTAB)
+			{
+			memcpy(&ShdrSym, &pShdr[i], sizeof(Elf32_Shdr));
+
+			// 查找字符串表头并拷贝出来备用
+			if (pShdr[ShdrSym.sh_link].sh_type == SHT_STRTAB)
+			{
+				memcpy(&ShdrStr, &pShdr[ShdrSym.sh_link], sizeof(Elf32_Shdr));
+				// found = 1;
+				break;
+			}
+			}
 		}
 	}
 	
