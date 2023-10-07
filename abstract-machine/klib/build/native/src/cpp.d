6bd46452e01ccb830b479a3bdef860b9a697bc8e
tracer-ysyx <tracer@ysyx.org>
2023-10-07 15:07:17 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:17 up  5:22,  2 users,  load average: 1.44, 1.04, 0.97

---
+++ abstract-machine/klib/build/native/src/cpp.d
@@ -0,0 +1,6 @@
+/home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/build/native/src/cpp.o: \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/src/cpp.c \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/arch/native.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/include/klib.h
---
+++ abstract-machine/klib/src/string.c
@@ -0,0 +1,112 @@
+#include <klib.h>
+#include <klib-macros.h>
+#include <stdint.h>
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+size_t strlen(const char *s) {
+  assert(s != NULL);
+  unsigned long i = 0;
+  while(s[i] != '\0'){
+    i++;
+  }
+  return i;
+}
+
+char *strcpy(char *dst, const char *src) {
+  assert(dst != NULL);
+  assert(src != NULL);
+  char *dst_t = dst;
+  while(*src != '\0'){
+    *dst_t++ = *src++;
+  }
+  *dst_t = '\0';
+  return dst;
+}
+
+char *strncpy(char *dst, const char *src, size_t n) {
+  assert(dst != NULL);
+  assert(src != NULL);
+  char *dst_t = dst;
+  for(; n; n--){
+    *dst_t++ = *src++;
+  }
+  *dst_t = '\0';
+  return dst;
+}
+
+char *strcat(char *dst, const char *src) {
+  char *dst_t = dst;
+  while(*dst_t != '\0'){
+    dst_t++;
+  }
+  while(*src != '\0'){
+    *dst_t++ = *src++;
+  }
+  *dst_t = '\0';
+  return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+  while(*s1 != '\0' && *s2 != '\0' && (*s1 == *s2)){
+    s1++;
+    s2++;
+  }
+  int t = *s1 - *s2;
+  return t;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+  while(n-1>0 && (*s1 == *s2)){
+    s1++;
+    s2++;
+    n--;
+  }
+  int t = *s1 - *s2;
+  return t;
+}
+
+void *memset(void *s, int c, size_t n) {
+  assert(s != NULL);
+  char *str = (char *)s;
+  for(;n;n--){
+    *str++ = c;
+  }
+  return s;
+}
+/* The memory areas may overlap: copying takes place as though the bytes in src are first
+       copied into a temporary array that does not overlap src or dest, and the bytes are then copied from the temporary array to dest*/
+// 从前往后会存在重叠情况，只要从后往前复制就不会出现了
+void *memmove(void *dst, const void *src, size_t n) {
+  char *dst_t = (char *) dst;
+  char *src_t = (char *) src;
+  dst_t = dst_t + n - 1;
+  src_t = src_t + n - 1;
+  for(;n;n--){
+    *dst_t-- = *src_t--;
+  }
+  return dst_t;
+}
+// memcpy本身不允许重叠
+void *memcpy(void *out, const void *in, size_t n) {
+  char *out_t = (char *) out;
+  char *in_t = (char *) in;
+  for(;n;n--){
+    *out_t++ = *in_t++;
+  }
+  return out;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+  char *s1_t = (char *) s1;
+  char *s2_t = (char *) s2;
+   while(n-1>0 && (*s1_t == *s2_t)){
+    s1_t++;
+    s2_t++;
+    n--;
+  }
+  int t = *s1_t - *s2_t;
+  return t;
+}
+
+#endif
