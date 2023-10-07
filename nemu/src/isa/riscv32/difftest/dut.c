94eec9b0ea9348f5eba5b060336f77bf95b15fdd
tracer-ysyx <tracer@ysyx.org>
2023-10-07 15:22:28 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:28 up  5:37,  2 users,  load average: 0.94, 0.97, 1.00

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,7 +19,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0; i<32; i++){
-    if(gpr(i) != ref_r->gpr[0]){
+    if(gpr(i) != ref_r->gpr[i]){
       printf("%d: 0x%08x  : 0x%08x  \n",i,gpr(i),ref_r->gpr[i]);
       return false;
     }
