f78d84270a4d2de2641fee28b45ea528c200ced2
jimple_ubuntu <jimple_ubuntu>
2023-10-08 19:19:57 +0800

    10.8backup

--- npc/vsrc/riscv_id_reg.v
+++ npc/vsrc/riscv_id_reg.v
@@ -1,48 +1,34 @@
-module ysyx_23060077_riscv_id_reg #(
-    ADDR_WIDTH = 1, 
-    DATA_WIDTH = 1
-) (
-    input                           clk,
+`include"riscv_define.vh"
+module ysyx_23060077_riscv_id_reg(
+    input 	    [`INST_WIDTH-1:0]       inst,
+    output reg 	[`REG_ADDR_WIDTH-1:0]   rs1,
+    output reg 	[`REG_ADDR_WIDTH-1:0]   rs2,
+    output reg 	[`REG_ADDR_WIDTH-1:0]   rd,
+    output reg 	                        rd_wen
+);
+wire [`REG_WIDTH-1:0] reg_zero;
+assign reg_zero = {`REG_WIDTH{1'b0}}
 
-    input                           rs1_en,
-    input         [ADDR_WIDTH-1:0]  rs1_addr,
-    output  reg   [DATA_WIDTH-1:0]  rs1_data,
-
-    input                           rs2_en,
-    input         [ADDR_WIDTH-1:0]  rs2_addr,
-    output  reg   [DATA_WIDTH-1:0]  rs2_data,
-
-    input                           rd_en,
-    input         [ADDR_WIDTH-1:0]  rd_addr,
-    output  reg   [DATA_WIDTH-1:0]  rd_data
+ysyx_23060077_riscv_mux#(
+  .NR_KEY      (11), 
+  .KEY_LEN     (7), 
+  .DATA_LEN    (`REG_WIDTH+`REG_WIDTH+REG_WIDTH+1)
+)riscv_mux_decode(
+  .key                (inst[6:0]),
+  .out                ({rs1,rs2,rd,rd_wen}),
+  .lut({    `LUI   ,  {reg_zero   , reg_zero   , inst[11:7], 1'b1},
+            `AUIPC ,  {reg_zero   , reg_zero   , inst[11:7], 1'b1},
+            `JAL   ,  {reg_zero   , reg_zero   , inst[11:7], 1'b1},
+            `JALR  ,  {inst[19:15], reg_zero   , inst[11:7], 1'b1},
+            `BRANCH,  {inst[19:15], inst[24:10], reg_zero  , 1'b0},
+            `LOAD  ,  {inst[19:15], reg_zero   , inst[11:7], 1'b1},
+            `STORE ,  {inst[19:15], inst[24:10], reg_zero  , 1'b0},
+            `OP_IMM,  {inst[19:15], reg_zero   , inst[11:7], 1'b1},
+            `OP    ,  {inst[19:15], inst[24:10], inst[11:7], 1'b1},
+            `FENCE ,  {reg_zero   , reg_zero   , reg_zero  , 1'b0},
+            `SYS   ,  {inst[19:15], reg_zero   , inst[11:7], 1'b0}
+  })
 );
 
-reg [DATA_WIDTH-1:0] memory [2**(ADDR_WIDTH)-1:0];
-
-integer i;
-initial begin
-    for(i=0; i<32; i=i+1)   memory[i] <= 0;
-end
-
-// read rs1
-always @(posedge clk) begin
-    if (rs1_en)begin
-        rs1_data <= memory[rs1_addr];
-    end 
-end
-
-// read rs2
-always @(posedge clk) begin
-    if (rs2_en)begin
-        rs2_data <= memory[rs2_addr];
-    end 
-end
-
-// write rd
-always @(posedge clk) begin
-    if (rd_en)begin
-        memory[rd_addr] <= rd_data;
-    end 
-end
 
 endmodule
