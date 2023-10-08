4977ee3446fac165382484adb490d0de5601e78c
tracer-ysyx <tracer@ysyx.org>
2023-10-08 21:51:32 +0800

    > generate verilog ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:32 up 12:18,  2 users,  load average: 1.79, 0.74, 0.62

--- npc/vsrc/riscv_id_opt.v
+++ npc/vsrc/riscv_id_opt.v
@@ -2,7 +2,7 @@
 module ysyx_23060077_riscv_id_opt(
     input 	    [`INST_WIDTH-1:0]       inst,
     output  	  [`ALU_OPT_WIDTH-1:0]    alu_opt,
-    output  	  [`ALU_OPT_WIDTH-1:0]    src_sel,
+    output  	  [`SRC_SEL_WIDTH-1:0]    src_sel,
     output  	  [`LSU_OPT_WIDTH-1:0]    lsu_opt
 );
 
@@ -10,21 +10,21 @@ module ysyx_23060077_riscv_id_opt(
 ysyx_23060077_riscv_mux#(
     NR_KEY      (11), 
     KEY_LEN     (10), 
-    DATA_LEN    (`REG_WIDTH+`REG_WIDTH+`REG_WIDTH+1)
+    DATA_LEN    (`ALU_OPT_WIDTH+`SRC_SEL_WIDTH+`LSU_OPT_WIDTH)
 )riscv_mux_decode(
   .key              ({inst[6:0],inst[14:12]}),  //opcode + func_code
   .out              ({rs1,rs2,rd,rd_wen}),
-  .lut({    `LUI   , {reg_zero   , reg_zero   , inst[11:7], 1'b1},
-            `AUIPC , {reg_zero   , reg_zero   , inst[11:7], 1'b1},
-            `JAL   , {reg_zero   , reg_zero   , inst[11:7], 1'b1},
-            `JALR  , {inst[19:15], reg_zero   , inst[11:7], 1'b1},
-            `BRANCH, {inst[19:15], inst[24:10], reg_zero  , 1'b0},
-            `LOAD  , {inst[19:15], reg_zero   , inst[11:7], 1'b1},
-            `STORE , {inst[19:15], inst[24:10], reg_zero  , 1'b0},
-            `OP_IMM, {inst[19:15], reg_zero   , inst[11:7], 1'b1},
-            `OP    , {inst[19:15], inst[24:10], inst[11:7], 1'b1},
-            `FENCE , {reg_zero   , reg_zero   , reg_zero  , 1'b0},
-            `SYS   , {inst[19:15], reg_zero   , inst[11:7], 1'b0}
+  .lut({  `LUI   , {},
+          `AUIPC , {},
+          `JAL   , {},
+          `JALR  , {},
+          `BRANCH, {},
+          `LOAD  , {},
+          `STORE , {},
+          `OP_IMM, {},
+          `OP    , {},
+          `FENCE , {},
+          `SYS   , {}
   })
 );
 
