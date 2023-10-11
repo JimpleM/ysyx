d84620b2fc55a80a22240b1e43d63e813db9cf56
tracer-ysyx <tracer@ysyx.org>
2023-10-11 16:29:27 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:29:27 up  7:12,  2 users,  load average: 0.51, 0.29, 0.33

--- npc/sim_src/include/debug.h
+++ npc/sim_src/include/debug.h
@@ -52,9 +52,8 @@
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
-      MUXDEF(CONFIG_TARGET_AM, printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
-        (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__))); \
-      IFNDEF(CONFIG_TARGET_AM, extern FILE* log_fp; fflush(log_fp)); \
+      printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ## __VA_ARGS__), \
+        (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##  __VA_ARGS__)); \
       assert(cond); \
     } \
   } while (0)
