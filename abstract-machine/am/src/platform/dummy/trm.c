5c01627b82fa25cc4a0e8c5fe92ec8f937bba850
tracer-ysyx <tracer@ysyx.org>
2023-10-11 19:04:45 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:45 up  9:47,  2 users,  load average: 0.33, 0.65, 0.54

--- abstract-machine/am/src/platform/dummy/trm.c
+++ abstract-machine/am/src/platform/dummy/trm.c
@@ -8,3 +8,7 @@ void putch(char ch) {
 void halt(int code) {
   while (1);
 }
+void _trm_init() {
+  int ret = main(mainargs);
+  halt(ret);
+}
--- am-kernels/tests/cpu-tests/tests/dummy.c
+++ am-kernels/tests/cpu-tests/tests/dummy.c
@@ -1,7 +1,3 @@
 int main() {
-  int a = 0;
-  if(a == 0){
-    return 0;
-  }
   return 0;
 }
