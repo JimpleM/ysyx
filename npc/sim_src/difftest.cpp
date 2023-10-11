e001472d1b1dd97110b75d6ad363928b0bb9f59e
tracer-ysyx <tracer@ysyx.org>
2023-10-11 21:33:21 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:21 up 12:16,  2 users,  load average: 0.75, 0.52, 0.48

--- npc/sim_src/difftest.cpp
+++ npc/sim_src/difftest.cpp
@@ -53,27 +53,27 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   ref_difftest_memcpy = (void(*)(paddr_t addr, void *buf, size_t n, bool direction))dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
 
-  ref_difftest_regcpy = (void(*)(void *dut, bool direction))dlsym(handle, "difftest_regcpy");
-  assert(ref_difftest_regcpy);
+//   ref_difftest_regcpy = (void(*)(void *dut, bool direction))dlsym(handle, "difftest_regcpy");
+//   assert(ref_difftest_regcpy);
 
-  ref_difftest_exec = (void(*)(uint64_t n))dlsym(handle, "difftest_exec");
-  assert(ref_difftest_exec);
+//   ref_difftest_exec = (void(*)(uint64_t n))dlsym(handle, "difftest_exec");
+//   assert(ref_difftest_exec);
 
-//   ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
-//   assert(ref_difftest_raise_intr);
+// //   ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
+// //   assert(ref_difftest_raise_intr);
 
-  void (*ref_difftest_init)() = (void (*)())dlsym(handle, "difftest_init");
-  assert(ref_difftest_init);
+//   void (*ref_difftest_init)(int) = (void (*)(int))dlsym(handle, "difftest_init");
+//   assert(ref_difftest_init);
 
-  Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
-  Log("The result of every instruction will be compared with %s. "
-      "This will help you a lot for debugging, but also significantly reduce the performance. "
-      "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
+//   Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
+//   Log("The result of every instruction will be compared with %s. "
+//       "This will help you a lot for debugging, but also significantly reduce the performance. "
+//       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
 
-  ref_difftest_init();
-  ref_difftest_memcpy(PMEM_LEFT, guest_to_host(PMEM_LEFT), img_size, DIFFTEST_TO_REF);
-  CPU_state cpu = package_cpu(cpu_gpr, PMEM_LEFT);
-  ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+//   ref_difftest_init(port);
+//   ref_difftest_memcpy(PMEM_LEFT, guest_to_host(PMEM_LEFT), img_size, DIFFTEST_TO_REF);
+//   CPU_state cpu = package_cpu(cpu_gpr, PMEM_LEFT);
+//   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
 static void checkregs() {
