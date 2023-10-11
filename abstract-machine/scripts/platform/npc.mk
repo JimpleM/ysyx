567729578b2b285a78c5eaffcdc17142555b5671
tracer-ysyx <tracer@ysyx.org>
2023-10-11 23:18:38 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:18:38 up 14:01,  2 users,  load average: 1.49, 1.28, 1.03

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -15,7 +15,7 @@ LDFLAGS   += --gc-sections -e _start
 
 
 
-NPCFLAGS += 
+NPCFLAGS += -d ${NEMU_HOME}/build/riscv32-nemu-interpreter-so
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
