b864225d0428144f67f81a5bd3ffbfe83e615aed
tracer-ysyx <tracer@ysyx.org>
2023-10-11 15:13:00 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:00 up  5:55,  2 users,  load average: 0.69, 0.54, 0.39

--- npc/vsrc/riscv_ex_alu.v
+++ npc/vsrc/riscv_ex_alu.v
@@ -17,7 +17,7 @@ assign sub_data = $signed(alu_a_data) - $signed(alu_b_data);
 assign {carry_flag,usub_data} = {1'b0,alu_a_data} - {1'b0,alu_b_data};
 
 riscv_mux#(
-  .NR_KEY      (11), 
+  .NR_KEY      (3), 
   .KEY_LEN     (4), 
   .DATA_LEN    (`DATA_WIDTH)
 )riscv_mux_ex_data(
@@ -26,14 +26,14 @@ riscv_mux#(
   .out              (alu_out_data),
   .lut({`ALU_ADD    , add_data,                                           
         `ALU_SUB    , sub_data,
-        `ALU_SLL    , alu_a_data << alu_b_data[5:0],
-        `ALU_SLT    , {{(`DATA_WIDTH-1){1'b0}},sub_data[`DATA_WIDTH-1]},
-        `ALU_SLTU   , {{(`DATA_WIDTH-1){1'b0}},carry_flag},
-        `ALU_XOR    , alu_a_data ^ alu_b_data,
-        `ALU_SRL    , alu_a_data >> alu_b_data[5:0],
-        `ALU_SRA    , {alu_a_data[`DATA_WIDTH-1],{alu_a_data[`DATA_WIDTH-2:0] >> alu_b_data[5:0]}},
-        `ALU_OR     , alu_a_data | alu_b_data,
-        `ALU_AND    , alu_a_data & alu_b_data,
+        // `ALU_SLL    , alu_a_data << alu_b_data[5:0],
+        // `ALU_SLT    , {{(`DATA_WIDTH-1){1'b0}},sub_data[`DATA_WIDTH-1]},
+        // `ALU_SLTU   , {{(`DATA_WIDTH-1){1'b0}},carry_flag},
+        // `ALU_XOR    , alu_a_data ^ alu_b_data,
+        // `ALU_SRL    , alu_a_data >> alu_b_data[5:0],
+        // `ALU_SRA    , {alu_a_data[`DATA_WIDTH-1],{alu_a_data[`DATA_WIDTH-2:0] >> alu_b_data[5:0]}},
+        // `ALU_OR     , alu_a_data | alu_b_data,
+        // `ALU_AND    , alu_a_data & alu_b_data,
         `ALU_SUBU   , usub_data
   })
 );
