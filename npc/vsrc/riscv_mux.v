bb6a73334ce328982479b59ca38d823077ee3e04
tracer-ysyx <tracer@ysyx.org>
2023-10-11 14:05:06 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:05:06 up  4:47,  2 users,  load average: 0.07, 0.13, 0.23

--- npc/vsrc/riscv_mux.v
+++ npc/vsrc/riscv_mux.v
@@ -48,5 +48,5 @@ module ysyx_23060077_riscv_mux_default #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) 
   input [DATA_LEN-1:0] default_out,
   input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
 );
-  ysyx_23060077_MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
+  ysyx_23060077_MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i1 (out, key, default_out, lut);
 endmodule
