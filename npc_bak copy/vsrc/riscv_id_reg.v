cdd9111f3d855a9674e17cce729a47dccdfcad6f
tracer-ysyx <tracer@ysyx.org>
2023-10-11 14:03:39 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:39 up  4:46,  2 users,  load average: 0.30, 0.17, 0.26

--- npc_bak copy/vsrc/riscv_id_reg.v
+++ npc_bak copy/vsrc/riscv_id_reg.v
@@ -7,10 +7,10 @@ module ysyx_23060077_riscv_id_reg(
     output reg 	                        rd_wen
 );
 wire [`REG_WIDTH-1:0] reg_zero;
-assign reg_zero = {`REG_WIDTH{1'b0}}
+assign reg_zero = {`REG_WIDTH{1'b0}};
 
 ysyx_23060077_riscv_mux#(
-  .NR_KEY      (11), 
+  .NR_KEY      (11),
   .KEY_LEN     (7), 
   .DATA_LEN    (`REG_WIDTH+`REG_WIDTH+REG_WIDTH+1)
 )riscv_mux_decode(
