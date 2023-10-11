e3d559c035a47da5e80ed950b8f3422c72a7af54
tracer-ysyx <tracer@ysyx.org>
2023-10-11 18:50:20 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:50:20 up  9:33,  2 users,  load average: 0.38, 0.35, 0.38

--- npc/sim_src/pmem.cpp
+++ npc/sim_src/pmem.cpp
@@ -1,6 +1,6 @@
 #include "include/risc32_lib.h"
 
-uint8_t pmem[PMEM_SIZE];
+uint8_t pmem[PMEM_SIZE] = {};
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - PMEM_LEFT; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + PMEM_LEFT; }
