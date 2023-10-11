5ed9a9b65b4ca91a905ec45a9489f5b02987092e
tracer-ysyx <tracer@ysyx.org>
2023-10-11 21:43:14 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:14 up 12:25,  2 users,  load average: 0.56, 0.38, 0.39

--- npc/sim_src/difftest.cpp
+++ npc/sim_src/difftest.cpp
@@ -81,7 +81,7 @@ void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
-void difftest_init(char *ref_so_file, long img_size) {
+void init_difftest(char *ref_so_file, long img_size,int port) {
   assert(ref_so_file != NULL);
 
   void *handle;
