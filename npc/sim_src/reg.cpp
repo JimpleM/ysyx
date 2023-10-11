40b3f3fdf94455496b5f1ce124cf81d0538e1168
tracer-ysyx <tracer@ysyx.org>
2023-10-11 20:00:24 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:00:24 up 10:43,  2 users,  load average: 0.06, 0.22, 0.34

--- npc/sim_src/reg.cpp
+++ npc/sim_src/reg.cpp
@@ -8,9 +8,9 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-uint64_t *cpu_gpr = NULL;
+uint32_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
-  cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
+  cpu_gpr = (uint32_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
 
 void isa_reg_display() {
