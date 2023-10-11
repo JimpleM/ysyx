8b0e9c9055a04c8cbdb771994bc2fc5c93960e64
tracer-ysyx <tracer@ysyx.org>
2023-10-11 18:38:54 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:38:54 up  9:21,  2 users,  load average: 0.64, 0.34, 0.37

--- npc/sim_src/include/common.h
+++ npc/sim_src/include/common.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 
 #ifdef ISA64
-typedef uint64_t word_t
-typedef int64_t sword_t
+typedef uint64_t word_t;
+typedef int64_t sword_t;
 #else
-typedef uint32_t word_t
-typedef int32_t sword_t
+typedef uint32_t word_t;
+typedef int32_t sword_t;
 #endif
 
 
