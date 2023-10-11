97da7e9ee536365d09a1c924af4ec4b7cb6c4fbe
tracer-ysyx <tracer@ysyx.org>
2023-10-11 20:44:34 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:34 up 11:27,  2 users,  load average: 1.24, 0.57, 0.40

--- npc/sim_src/include/risc32_lib.h
+++ npc/sim_src/include/risc32_lib.h
@@ -23,5 +23,7 @@ typedef struct{
 
 void init_npc(int argc, char *argv[]);
 
+//pmem
+uint8_t* guest_to_host(paddr_t paddr)
 uint32_t pmem_read(uint32_t addr, int len);
 void pmem_write(uint32_t addr, uint32_t data, int len);
