221416cccfe3fec2a4b4478f64cac6555c9dc837
tracer-ysyx <tracer@ysyx.org>
2023-10-12 17:08:43 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:08:43 up  8:13,  9 users,  load average: 1.18, 1.35, 1.25

--- test/test.c
+++ test/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
 	for(int i=0; i<32;i++){
-		printf("assign gpr[%d] = rd_en &&(rd_data == 5'd%d) ? rd_data : gpr[%d];\n",i,i,i);
+		printf("assign gpr[%d] = rd_en &&(rd_addr == 5'd%d) ? rd_data : gpr[%d];\n",i,i,i);
 	}
 	
 	return 0;
