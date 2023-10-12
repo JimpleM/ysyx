e5c6eef4599edc553fda0810693874c80269a19f
tracer-ysyx <tracer@ysyx.org>
2023-10-12 09:13:28 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:13:28 up 17 min,  1 user,  load average: 0.97, 1.00, 0.79

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -27,18 +27,18 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  CPU_state *temp = (CPU_state *)dut;
-  if(direction == DIFFTEST_TO_REF){
-    for(int i=0; i<32; i++){
-      cpu.gpr[i] = temp->gpr[i];
-    }
-    cpu.pc = temp->pc;
-  }else{
-    for(int i=0; i<32; i++){
-      temp->gpr[i] = cpu.gpr[i];
-    }
-    temp->pc = cpu.pc;
-  }
+  // CPU_state *temp = (CPU_state *)dut;
+  // if(direction == DIFFTEST_TO_REF){
+  //   for(int i=0; i<32; i++){
+  //     cpu.gpr[i] = temp->gpr[i];
+  //   }
+  //   cpu.pc = temp->pc;
+  // }else{
+  //   for(int i=0; i<32; i++){
+  //     temp->gpr[i] = cpu.gpr[i];
+  //   }
+  //   temp->pc = cpu.pc;
+  // }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
