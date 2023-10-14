427137cc3324d56aab069432f8087d7fb764f29b
tracer-ysyx <tracer@ysyx.org>
2023-10-14 16:23:19 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:23:19 up 38 min,  1 user,  load average: 1.10, 1.27, 1.34

--- npc/sim_src/include/reg.h
+++ npc/sim_src/include/reg.h
@@ -8,5 +8,6 @@
 void isa_reg_display();
 CPU_state package_cpu(uint32_t *gpr, uint32_t pc);
 bool isa_difftest_checkregs(CPU_state *ref);
+word_t isa_reg_str2val(const char *s, bool *success);
 
 #endif
--- npc/sim_src/sdb/reg.cpp
+++ npc/sim_src/sdb/reg.cpp
@@ -55,3 +55,20 @@ CPU_state package_cpu(uint32_t *gpr, uint32_t pc){
     }
     return cpu;
 }
+
+word_t isa_reg_str2val(const char *s, bool *success) {
+  if(strcmp(s,"pc") == 0){
+    // printf("$pc : 0x%08x\n",cpu.pc);
+    return cpu.pc;
+  }
+  for(int i=0; i<32; i++){
+    if(strcmp(s,regs[i]) == 0){
+    //printf("%3s : 0x%08x  ",regs[i],cpu.gpr[i]);
+      return cpu.gpr[i];
+    }
+  }
+  printf("no such reg:%s\n",s);
+  assert(0);
+  *success = false;
+  return 0;
+}
