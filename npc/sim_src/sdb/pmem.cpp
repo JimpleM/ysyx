a1b1fd97422299a7d865b95753c6b3e4016f3aa3
tracer-ysyx <tracer@ysyx.org>
2023-10-14 16:42:29 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:29 up 57 min,  1 user,  load average: 1.10, 1.16, 1.18

--- npc/sim_src/sdb/pmem.cpp
+++ npc/sim_src/sdb/pmem.cpp
@@ -49,13 +49,13 @@ void pmem_write(uint32_t addr, uint32_t data, int len){
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  // IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
