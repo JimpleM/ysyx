1dd77c407423fa5e67bdd45c0fe0516c22bb272a
tracer-ysyx <tracer@ysyx.org>
2023-10-16 16:10:21 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:10:21 up  6:56,  1 user,  load average: 0.99, 1.68, 1.60

--- npc/sim_src/sdb/disasm.cc
+++ npc/sim_src/sdb/disasm.cc
@@ -12,6 +12,7 @@
 *
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
+#include "disasm.h"
 
 #if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
