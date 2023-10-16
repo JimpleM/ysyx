1d720593e37a494a4bd901ad21f8bc6e408e4de4
tracer-ysyx <tracer@ysyx.org>
2023-10-16 15:28:53 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:53 up  6:15,  1 user,  load average: 0.90, 0.95, 1.01

--- npc/sim_src/sdb/cpu.cpp
+++ npc/sim_src/sdb/cpu.cpp
@@ -49,7 +49,7 @@ static void exec_once() {
   printf("%x\n",cpu_inst);
   cpu_inst = pmem_read((uint32_t)cpu_pc,4);
 
-  disassemble(p, sizeof(p),(uint64_t)cpu_pc, (uint8_t *)&cpu_inst, 4);
+  // disassemble(p, sizeof(p),(uint64_t)cpu_pc, (uint8_t *)&cpu_inst, 4);
   // printf("%s\n",p);
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
