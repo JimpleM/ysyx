e71e81b1c4da2a4f64e26f369021d7f2fc1782a6
tracer-ysyx <tracer@ysyx.org>
2023-10-16 20:27:23 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:27:23 up 11:13,  2 users,  load average: 0.84, 0.88, 0.99

--- test/hello.c
+++ test/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <inttypes.h>
 int main(){
-    printf("Hello World!\n");
+    uint32_t a = 0x80000000;
+    printf("%x\n",a-1);
+    printf("%x\n",-1);
     return 0;
 }
