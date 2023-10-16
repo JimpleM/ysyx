664a6dbd45872ba5ccf56d9582e4512749555067
tracer-ysyx <tracer@ysyx.org>
2023-10-16 17:36:24 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:24 up  8:23,  1 user,  load average: 1.22, 1.16, 1.16

--- npc/sim_src/sdb/dpic.cpp
+++ npc/sim_src/sdb/dpic.cpp
@@ -31,8 +31,10 @@ extern "C" void get_riscv32_rst(svBit rst_n) {
   }
 }
 
-int addr = 0;
-int data = 0;
+int raddr = 0;
+int rdata = 0;
+int waddr = 0;
+int wdata = 0;
 
 extern "C" void riscv_pmem_read(int raddr, int *rdata, svBit ren){
 	if(ren){
@@ -41,12 +43,12 @@ extern "C" void riscv_pmem_read(int raddr, int *rdata, svBit ren){
 			stop_flag = 1;
 		}
 #ifdef CONFIG_MTRACE
-	if(addr != raddr && data != *rdata){
+	if(raddr != raddr && rdata != *rdata){
 		if(raddr >= CONFIG_MTRACE_START_ADDR && raddr <= CONFIG_MTRACE_END_ADDR){
 			printf("read address:%08x data:%08x\n",raddr,*rdata);
 		}
-		addr = raddr;
-		data = *rdata;
+		raddr = raddr;
+		rdata = *rdata;
 	}
 #endif
 	}
@@ -55,12 +57,12 @@ extern "C" void riscv_pmem_read(int raddr, int *rdata, svBit ren){
 extern "C" void riscv_pmem_write(int waddr, int wdata, int wmask){
 	pmem_write((uint32_t)waddr,(uint32_t)wdata,wmask);
 #ifdef CONFIG_MTRACE
-	if(addr != waddr && data != wdata){
+	if(waddr != waddr && wdata != wdata){
 		if(waddr >= CONFIG_MTRACE_START_ADDR && waddr <= CONFIG_MTRACE_END_ADDR){
 			printf("write address:%08x data:%08x\n",waddr,wdata);
 		}
-		addr = waddr;
-		data = wdata;
+		waddr = waddr;
+		wdata = wdata;
 	}
 #endif
 	
