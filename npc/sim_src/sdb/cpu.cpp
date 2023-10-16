865e26c797e78cadb2b781828c9bc3006c44f071
tracer-ysyx <tracer@ysyx.org>
2023-10-16 15:23:13 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:23:13 up  6:09,  1 user,  load average: 0.94, 1.10, 1.08

--- npc/sim_src/sdb/cpu.cpp
+++ npc/sim_src/sdb/cpu.cpp
@@ -46,7 +46,8 @@ static void exec_once() {
     char p[100];
 #ifdef CONFIG_ITRACE
   cpu_inst = pmem_read((uint32_t)cpu_pc,4);
-  printf("%d\n",cpu_inst);
+  printf("%x\n",cpu_pc);
+  printf("%x\n",cpu_inst);
   disassemble(p, sizeof(p),(uint64_t)cpu_pc, (uint8_t *)&cpu_inst, 4);
   // printf("%s\n",p);
 #else
