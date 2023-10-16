5561ebdf5451eab1638c948d132716666bbd5ea5
tracer-ysyx <tracer@ysyx.org>
2023-10-16 15:47:58 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:47:58 up  6:34,  1 user,  load average: 1.04, 0.99, 1.01

--- npc/sim_src/sdb/disasm.cpp
+++ npc/sim_src/sdb/disasm.cpp
@@ -98,9 +98,9 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
-  printf("afsf");
+  printf("afsf\n");
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
-    printf("afsf");
+  printf("afsf\n");
 
   std::string s;
   raw_string_ostream os(s);
