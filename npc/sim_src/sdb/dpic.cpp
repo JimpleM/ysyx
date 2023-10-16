29f398bad0a7742900fd45135838e3eddf2fceab
tracer-ysyx <tracer@ysyx.org>
2023-10-16 17:30:44 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:44 up  8:17,  1 user,  load average: 1.13, 1.19, 1.17

--- npc/sim_src/sdb/dpic.cpp
+++ npc/sim_src/sdb/dpic.cpp
@@ -45,8 +45,8 @@ extern "C" void riscv_pmem_read(int raddr, int *rdata, svBit ren){
 		if(raddr >= CONFIG_MTRACE_START_ADDR && raddr <= CONFIG_MTRACE_END_ADDR){
 			printf("read address:%08x data:%08x\n",raddr,*rdata);
 		}
-		addr = raddr;
-		data = rdata;
+		addr = (uint32_t)raddr;
+		data =  (uint32_t)rdata;
 	}
 #endif
 	}
@@ -59,8 +59,8 @@ extern "C" void riscv_pmem_write(int waddr, int wdata, int wmask){
 		if(waddr >= CONFIG_MTRACE_START_ADDR && waddr <= CONFIG_MTRACE_END_ADDR){
 			printf("write address:%08x data:%08x\n",waddr,wdata);
 		}
-		addr = waddr;
-		data = wdata;
+		addr = (uint32_t)waddr;
+		data = (uint32_t)wdata;
 	}
 #endif
 	
