e27e917edec2b814240662aa879e277fb5e74539
tracer-ysyx <tracer@ysyx.org>
2023-10-16 15:46:57 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:57 up  6:33,  1 user,  load average: 0.89, 0.96, 1.01

--- npc/sim_src/sdb/cpu.cpp
+++ npc/sim_src/sdb/cpu.cpp
@@ -100,7 +100,11 @@ static void execute(uint64_t n) {
       
         //反汇编结果
       #ifdef CONFIG_ITRACE
+          printf("afsf");
+
         cpu_inst = pmem_read((uint32_t)cpu_pc,4);
+          printf("afsf");
+
         disassemble(dis_str, sizeof(dis_str),pc, (uint8_t *)&inst_data, 4);
         // printf("%s\n",p);
       #else
