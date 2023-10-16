a44dfdc3defdf59c0e02c902954fde5f5d9257c0
tracer-ysyx <tracer@ysyx.org>
2023-10-16 15:09:09 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:09:09 up  5:55,  1 user,  load average: 1.09, 1.04, 1.08

--- npc/sim_src/sdb/pmem.cpp
+++ npc/sim_src/sdb/pmem.cpp
@@ -11,8 +11,7 @@ word_t host_read(void *addr, int len) {
     case 2: return *(uint16_t *)addr;
     case 4: return *(uint32_t *)addr;
     // IFDEF(CONFIG_ISA64, case 8: return *(uint64_t *)addr);
-    // default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
-    default: assert(0);
+    default: MUXDEF(CONFIG_RT_CHECK, assert(0), return 0);
   }
 }
 
@@ -22,7 +21,7 @@ void host_write(void *addr, int len, word_t data) {
     case 2: *(uint16_t *)addr = data; return;
     case 4: *(uint32_t *)addr = data; return;
     // IFDEF(CONFIG_ISA64, case 8: *(uint64_t *)addr = data; return);
-    // IFDEF(CONFIG_RT_CHECK, default: assert(0));
+    IFDEF(CONFIG_RT_CHECK, default: assert(0));
     // default: assert(0);
   }
 }
