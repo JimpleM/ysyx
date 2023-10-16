e6e711857b64e477783ea703d765dd502a1084a8
tracer-ysyx <tracer@ysyx.org>
2023-10-16 16:10:39 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:10:39 up  6:57,  1 user,  load average: 0.99, 1.64, 1.59

--- npc/sim_src/include/disasm.h
+++ npc/sim_src/include/disasm.h
@@ -5,7 +5,7 @@
 
 
 //disasm
-void init_disasm(const char *triple);
+extern "C" void init_disasm(const char *triple);
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 
 
