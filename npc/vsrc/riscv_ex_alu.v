0913bb30bfbe78527a823f0c5d9300dcc86c728e
tracer-ysyx <tracer@ysyx.org>
2023-10-16 21:09:53 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:53 up 11:56,  2 users,  load average: 0.98, 1.06, 1.03

--- npc/vsrc/riscv_ex_alu.v
+++ npc/vsrc/riscv_ex_alu.v
@@ -14,7 +14,7 @@ wire [`DATA_WIDTH-1:0] usub_data;
 wire [`DATA_WIDTH-1:0] temp_b;
 wire a;
 
-assign temp_b = (alu_opt == `ALU_SUB)&&alu_b_data[`DATA_WIDTH-1] ? ~alu_b_data:alu_b_data;
+assign temp_b = (alu_opt == `ALU_SUB)? ~alu_b_data:alu_b_data;
 
 assign add_data = alu_a_data + alu_b_data;
 //相当与变成有符号数进行减法，看最高bit
