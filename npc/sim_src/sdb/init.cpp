99621293feae916dd498a9ef4ad6b7410271af21
tracer-ysyx <tracer@ysyx.org>
2023-10-17 14:28:52 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:52 up  1:30,  2 users,  load average: 0.96, 1.22, 1.23

--- npc/sim_src/sdb/init.cpp
+++ npc/sim_src/sdb/init.cpp
@@ -54,7 +54,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'd': diff_so_file = optarg; break;
       case 'i': img_file = optarg; break;
       case 'e': elf_file = optarg; break;
-      case 1: img_file = optarg; return 0;
+      // case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-l,--log=FILE           output log to FILE\n");
