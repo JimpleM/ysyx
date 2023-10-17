e243c39e2d14625c1d92eb5c7100c0229a060f72
tracer-ysyx <tracer@ysyx.org>
2023-10-17 17:22:41 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:41 up  4:24,  2 users,  load average: 1.22, 1.39, 1.33

--- npc/vsrc/riscv_ex_alu.v
+++ npc/vsrc/riscv_ex_alu.v
@@ -6,25 +6,6 @@ module riscv_ex_alu(
     output  	      [`DATA_WIDTH-1:0]       alu_out_data
 );
 
-// wire sub_flag = (alu_opt == `ALU_SUB || alu_opt == `ALU_SLT || alu_opt == `ALU_SLTU);
-
-// wire [`DATA_WIDTH:0] alu_a_t = {alu_a_data[`DATA_WIDTH-1],alu_a_data};
-// wire [`DATA_WIDTH:0] alu_b_t = {alu_b_data[`DATA_WIDTH-1],alu_b_data} ^ {(`DATA_WIDTH+1){sub_flag}};
-// wire [`DATA_WIDTH:0] cin     = {{(`DATA_WIDTH){1'b0}},sub_flag};
-// wire [`DATA_WIDTH:0] add_out = alu_a_t + alu_b_t + cin;
-
-// wire top_A = alu_a_t[`DATA_WIDTH];
-// wire top_B = alu_b_t[`DATA_WIDTH];
-// wire top_C = add_out[`DATA_WIDTH];
-
-// wire sign_flag = add_out[`DATA_WIDTH-1];
-// wire over_flag = add_out[`DATA_WIDTH] ^ add_out[`DATA_WIDTH-1];
-// wire carry_flag = ((top_A|top_B|top_C) & (top_A|!top_B|!top_C) & (!top_A|top_B|!top_C) & (!top_A|!top_B|top_C)) ^ sub_flag;
-
-
-wire [`DATA_WIDTH-1:0] sra_result = {{{(`DATA_WIDTH){alu_a_data[`DATA_WIDTH-1]}},alu_a_data} >> alu_b_data[5:0]}[`DATA_WIDTH-1:0] ;
-
-
 wire sub_flag = (alu_opt == `ALU_SUB || alu_opt == `ALU_SLT || alu_opt == `ALU_SLTU);
 
 wire [`DATA_WIDTH:0] alu_a_t = {alu_a_data[`DATA_WIDTH-1],alu_a_data};
@@ -32,13 +13,20 @@ wire [`DATA_WIDTH:0] alu_b_t = {alu_b_data[`DATA_WIDTH-1],alu_b_data} ^ {(`DATA_
 wire [`DATA_WIDTH:0] cin     = {{(`DATA_WIDTH){1'b0}},sub_flag};
 wire [`DATA_WIDTH:0] add_out = alu_a_t + alu_b_t + cin;
 
-wire top_A = alu_a_t[`DATA_WIDTH-1];
-wire top_B = alu_b_t[`DATA_WIDTH-1];
+wire top_A = alu_a_t[`DATA_WIDTH];
+wire top_B = alu_b_t[`DATA_WIDTH];
 wire top_C = add_out[`DATA_WIDTH];
 
 wire sign_flag = add_out[`DATA_WIDTH-1];
 wire over_flag = add_out[`DATA_WIDTH] ^ add_out[`DATA_WIDTH-1];
-wire carry_flag = add_out[`DATA_WIDTH];
+/*
+减法的时候  
+*/
+// wire carry_flag = ((top_A|top_B|top_C) & (top_A|!top_B|!top_C) & (!top_A|top_B|!top_C) & (!top_A|!top_B|top_C)) ^ sub_flag;
+wire carry_flag = (top_A&!top_C&!sub_flag) | (!top_A&top_B&!top_C) | (!top_A&top_C&sub_flag) | (top_A|top_B|top_C);
+
+
+wire [`DATA_WIDTH-1:0] sra_result = {{{(`DATA_WIDTH){alu_a_data[`DATA_WIDTH-1]}},alu_a_data} >> alu_b_data[5:0]}[`DATA_WIDTH-1:0] ;
 
 
 
