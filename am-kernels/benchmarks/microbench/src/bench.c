ab1d3c10d533dfd442705d5269ead9a917acfda8
tracer-ysyx <tracer@ysyx.org>
2023-10-18 16:37:22 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:37:22 up  7:42,  2 users,  load average: 0.00, 0.07, 0.23

--- am-kernels/benchmarks/microbench/src/bench.c
+++ am-kernels/benchmarks/microbench/src/bench.c
@@ -14,8 +14,6 @@ static char *format_time(uint64_t us) {
   static char buf[32];
   uint32_t ms = us / 1000;
   us -= ms * 1000;
-  printf("%d\n",us);
-  printf("%ld\n",us);
   assert(us < 1000);
   int len = sprintf(buf, "%d.000", ms);
   char *p = &buf[len - 1];
