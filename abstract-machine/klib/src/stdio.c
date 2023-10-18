e454a1768f4e0822dd46ad9122ffcc10943fb1c7
tracer-ysyx <tracer@ysyx.org>
2023-10-18 14:55:00 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:00 up  6:00,  2 users,  load average: 0.00, 0.05, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -105,7 +105,7 @@ int printf(const char *fmt, ...) {
   char str[1024];
   va_list args;
   va_start(args,fmt);
-  debug_printf("%s\n",fmt);
+  debug_printf("%s",fmt);
   debug_printf(fmt,args);
   int count = sprintf(str,fmt,args);
   // for(int i=0; i<count; i++){
