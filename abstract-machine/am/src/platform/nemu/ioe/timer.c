98b55bb643cd597965d59bc3e176e9c77588c811
tracer-ysyx <tracer@ysyx.org>
2023-10-18 19:26:51 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:51 up 10:32,  2 users,  load average: 0.01, 0.07, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,7 @@
 static uint64_t boot_time = 0;
 
 static uint64_t read_time(){
-  printf("%ld\n",(uint64_t)inl(RTC_ADDR)+ (uint64_t)inl(RTC_ADDR+4)<<32);
+  printf("%ld\n",(uint64_t)inl(RTC_ADDR)+ ((uint64_t)inl(RTC_ADDR+4))<<32);
   printf("%d\n",inl(RTC_ADDR) | (uint64_t)inl(RTC_ADDR+4)<<32);
   return ((uint64_t)inl(RTC_ADDR) + (uint64_t)inl(RTC_ADDR+4)<<32);
 }
