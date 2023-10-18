ccc009cd4014ef5dd136932a950e320a8b1ffcfd
tracer-ysyx <tracer@ysyx.org>
2023-10-18 18:38:14 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:38:14 up  9:43,  2 users,  load average: 0.00, 0.06, 0.09

--- am-kernels/benchmarks/microbench/src/bench.c
+++ am-kernels/benchmarks/microbench/src/bench.c
@@ -9,11 +9,12 @@ Setting *setting;
 static char *hbrk;
 
 static uint64_t uptime() { 
-  uint64_t temp = io_read(AM_TIMER_UPTIME).us;
-  printf("%ld\n",temp);
+  AM_TIMER_UPTIME_T temp = io_read(AM_TIMER_UPTIME);
+  printf("%ld\n",temp.us);
 
   AM_TIMER_RTC_T rtc = io_read(AM_TIMER_RTC);
   printf("%d\n",rtc.year);
+
   return io_read(AM_TIMER_UPTIME).us; 
 }
 
