c4b40be616b87542a1aeb75a1640372bcd138372
tracer-ysyx <tracer@ysyx.org>
2023-10-18 15:59:27 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:59:27 up  7:04,  2 users,  load average: 0.05, 0.12, 0.15

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -132,6 +132,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   int ArgInt = 0;           // 接收整型
   unsigned int ArgUInt = 0; // 接收无符号
   unsigned long ArgHex = 0; // 接收十六进制
+  unsigned long ArgLong = 0;
   double  ArgFloat = 0;     // 接收浮点数
 
   char Sign = '\0';
@@ -192,6 +193,11 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
       }else if(*fmt == 'x'){
         ArgHex = va_arg(ap, unsigned long);
         number_to_str(str_temp,(ul)ArgHex,16);
+        out_t = insert_space(out_t,num_before_dig-strlen(str_temp));
+        strcat_out(out_t,str_temp);
+      }else if(*fmt == 'l'){
+        ArgLong = va_arg(ap, unsigned long);
+        number_to_str(str_temp,(ul)ArgLong,16);
         out_t = insert_space(out_t,num_before_dig-strlen(str_temp));
         strcat_out(out_t,str_temp);
       }else if(*fmt == 'f'){
