ea4ee3cafaadad5a8eb3203680552c423b5c22c1
tracer-ysyx <tracer@ysyx.org>
2023-10-18 17:02:38 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:02:38 up  8:08,  2 users,  load average: 0.31, 0.09, 0.11

--- am-kernels/benchmarks/microbench/src/bench.c
+++ am-kernels/benchmarks/microbench/src/bench.c
@@ -51,6 +51,7 @@ static void bench_reset() {
 
 static void bench_done(Result *res) {
   res->usec = uptime() - res->usec;
+  printf("%x\n",res->usec);
 }
 
 static const char *bench_check(Benchmark *bench) {
