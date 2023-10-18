889cc6b46b9e21a991a3472eb88be726855c1dfe
tracer-ysyx <tracer@ysyx.org>
2023-10-18 15:45:29 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:29 up  6:51,  2 users,  load average: 0.02, 0.21, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -112,7 +112,10 @@ int printf(const char *fmt, ...) {
   // debug_printf("%s",fmt);
   // debug_printf(fmt,args);
   int count = vsprintf(str,fmt,args);
-  debug_printf("%d\n",count);
+  for(int i=0; i<strlen(str); i++){
+    putch(str[i]);
+  }
+  sprintf(str,"%d",count);
   for(int i=0; i<strlen(str); i++){
     putch(str[i]);
   }
@@ -236,118 +239,17 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 
-// int sprintf(char *out, const char *fmt, ...) {
-//   assert(fmt != NULL);
+int sprintf(char *out, const char *fmt, ...) {
+  assert(fmt != NULL);
 
-//   char str_temp[100];
-//   unsigned long num_temp;
+  va_list args;
+  va_start(args,fmt);
 
-//   char* ArgStr = NULL;      // 接收字符型
-//   int ArgInt = 0;           // 接收整型
-//   unsigned int ArgUInt = 0; // 接收无符号
-//   unsigned long ArgHex = 0; // 接收十六进制
-//   double  ArgFloat = 0;     // 接收浮点数
+  int count = vsprintf(out,fmt,args);
 
-//   char Sign = '\0';
-//   int num_before_dig = 0;
-//   int num_after_dig = 0;
-
-//   va_list args;
-//   va_start(args,fmt);
-
-//   while(*fmt != '\0'){
-//     if(*fmt == '%'){
-//       fmt++;
-//       if(*fmt == '%'){
-//         *out++ = '%';
-//         fmt++;
-//         continue;
-//       }
-
-//       if(*fmt == '-' || *fmt == '+' || *fmt == ' '){
-//         Sign = *fmt;
-//         fmt++;
-//       }
-
-//       fmt = parse_number(fmt,&num_before_dig);
-//       if(*fmt == '.'){
-//         fmt++;
-//         fmt = parse_number(fmt,&num_after_dig);
-//       }
-
-//       if(*fmt == 's'){
-//         ArgStr = va_arg(args, char*);
-//         // assert(ArgStr != NULL);
-//         strcat_out(ArgStr);
-//       }else if(*fmt == 'c'){
-//         ArgInt = va_arg(args, int);
-//         *out++ = ArgInt;
-//       }else if(*fmt == 'd'){
-//         ArgInt = va_arg(args, int);
-//         ArgStr = str_temp;
-
-//         if(ArgInt < 0){
-//           *ArgStr++ = '-';
-//           ArgUInt = ArgInt;
-//           ArgUInt = (~ArgUInt)+1;
-//         }else{
-//           if(Sign == '+' || Sign == ' '){
-//             *ArgStr++ = Sign;
-//           }
-//           ArgUInt = ArgInt;
-//           Sign = '\0';
-//         }
-//         number_to_str(ArgStr,ArgUInt,10);
-//         out = insert_space(out,num_before_dig-strlen(str_temp));
-       
-//         strcat_out(str_temp);
-//       }
-//       else if(*fmt == 'u'){
-//         ArgUInt = va_arg(args, unsigned int);
-//         number_to_str(str_temp,(ul)ArgUInt,10);
-//         out = insert_space(out,num_before_dig-strlen(str_temp));
-//         strcat_out(str_temp);
-//       }else if(*fmt == 'x'){
-//         ArgHex = va_arg(args, unsigned long);
-//         number_to_str(str_temp,(ul)ArgHex,16);
-//         out = insert_space(out,num_before_dig-strlen(str_temp));
-//         strcat_out(str_temp);
-//       }else if(*fmt == 'f'){
-//         ArgFloat = va_arg(args, double);
-//         ArgStr = str_temp;
-        
-//         // if(ArgFloat < 0){
-//         //   *ArgStr++ = '-';
-//         //   ArgFloat = -ArgFloat;
-//         // }
-
-//         // num_temp = (ul)ArgFloat;
-//         // ArgFloat = ArgFloat - num_temp;
-//         //整数位
-//         // ArgStr = number_to_str(ArgStr,(ul)num_temp,10);
-//         //小数点
-//         *ArgStr++ = '.';
-//         *ArgStr = '\0';
-//         //小数位
-//         // num_temp = num_after_dig == 0 ? (ArgFloat + 1e-6)* 1000000 : ArgFloat*(my_pow(10,num_after_dig))+0.5;
-//         // ArgStr = number_to_str(ArgStr,(ul)num_temp,10);
-
-//         // out = insert_space(out,num_before_dig-strlen(str_temp));
-//         // strcat_out(str_temp);
-//       }
-
-//       num_before_dig = 0;
-//       num_after_dig = 0;
-//     }else{
-//       *out++ = *fmt;
-//     }
-//     fmt++;
-//   }
-  
-//   *out++ = '\0';
-//   va_end(args);
-//   return strlen(out);
-// }
+  va_end(args);
+  return count;
+}
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
