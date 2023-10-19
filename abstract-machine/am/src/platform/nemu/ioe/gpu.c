0ede97a22d61adcc4c1fd385ddfca150562cbeac
tracer-ysyx <tracer@ysyx.org>
2023-10-19 20:42:00 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:00 up 11:15,  3 users,  load average: 0.02, 0.11, 0.15

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,18 +4,32 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+
+  AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
+  int w = info.width, h = info.height;
+
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
+  int w = info.width, h = info.height;
+
   *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .present = true, 
+    .has_accel = false,
+    .width = w, 
+    .height = h,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
+    // ctl->x = 
     outl(SYNC_ADDR, 1);
   }
 }
