18f8729c7434cf7207216a3b50bf2d6673063192
tracer-ysyx <tracer@ysyx.org>
2023-10-19 21:12:35 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:35 up 11:46,  3 users,  load average: 1.17, 1.06, 0.64

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,7 +8,7 @@ void __am_gpu_init() {
   int i;
 
   AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
-  int w = 400, h = 300;
+  int w = info.width, h = info.height;
 
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
@@ -23,21 +23,24 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, 
     .has_accel = false,
-    .width = 400, 
-    .height = 300,
-    .vmemsz = 400 * 300 * sizeof(uint32_t)
+    .width = w, 
+    .height = h,
+    .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int i;
-  int x = ctl->x, y = ctl->y;
-  int w = ctl->w, h = ctl->h;
-  uint32_t *pixels = (uint32_t *)ctl->pixels;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w*h; i ++){
-    fb[i] = pixels[i] & 0x00ffffff;
-  } 
+  // int i,j;
+  // int x = ctl->x, y = ctl->y;
+  // int w = ctl->w, h = ctl->h;
+  // uint32_t *pixels = (uint32_t *)ctl->pixels;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w; i ++){
+  //   for(j = 0; j < h; j++){
+
+  //   }
+  //   fb[i] = pixels[i] & 0x00ffffff;
+  // } 
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
