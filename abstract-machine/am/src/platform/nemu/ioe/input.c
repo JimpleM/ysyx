fd6524f15b5917a9b46e3c45d828d3748941ffba
tracer-ysyx <tracer@ysyx.org>
2023-10-19 19:14:47 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:14:47 up  9:48,  2 users,  load average: 0.16, 0.30, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -8,5 +8,5 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t ev = inl(KBD_ADDR);
   kbd->keydown = (ev & KEYDOWN_MASK) != 0;
   kbd->keycode = ev & (~KEYDOWN_MASK);
-  printf("%d %d\n",kbd->keydown,kbd->keycode);
+  // printf("%d %d\n",kbd->keydown,kbd->keycode);
 }
