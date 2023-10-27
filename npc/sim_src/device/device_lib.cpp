7c869fae6f8fb821082e436fd2a59c2daaca8bb1
tracer-ysyx <tracer@ysyx.org>
2023-10-27 18:01:05 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:01:05 up  8:47,  1 user,  load average: 0.43, 0.40, 0.31

--- npc/sim_src/device/device_lib.cpp
+++ npc/sim_src/device/device_lib.cpp
@@ -18,7 +18,7 @@ uint32_t device_read(uint32_t addr){
 
 void device_write(uint32_t addr, uint32_t data){
     count++;
-    if(addr == SERIAL_PORT && count >=2){
+    if(addr == SERIAL_PORT && count >=3){
         count = 0;
         uart_write(data);
         return ;
