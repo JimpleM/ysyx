fbcad5565c90dc61452b61c75f378d1d117925ba
tracer-ysyx <tracer@ysyx.org>
2023-10-27 15:17:50 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:17:50 up  6:04,  1 user,  load average: 0.03, 0.16, 0.22

---
+++ npc/sim_src/device/include/device.h
@@ -0,0 +1,47 @@
+#ifndef DEVIDE_H__
+#define DEVIDE_H__
+
+#include "common.h"
+#include <SDL2/SDL.h>
+
+// # define DEVICE_BASE 0xa0000000
+
+// #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
+// #define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+// #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+// #define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
+// #define AUDIO_ADDR      (DEVICE_BASE + 0x0000200)
+// #define DISK_ADDR       (DEVICE_BASE + 0x0000300)
+// #define FB_ADDR         (MMIO_BASE   + 0x1000000)
+// #define AUDIO_SBUF_ADDR (MMIO_BASE   + 0x1200000)
+
+// keyboard
+
+// Note that this is not the standard
+#define KEYDOWN_MASK 0x8000
+#define _KEYS(f) \
+  f(ESCAPE) f(F1) f(F2) f(F3) f(F4) f(F5) f(F6) f(F7) f(F8) f(F9) f(F10) f(F11) f(F12) \
+f(GRAVE) f(1) f(2) f(3) f(4) f(5) f(6) f(7) f(8) f(9) f(0) f(MINUS) f(EQUALS) f(BACKSPACE) \
+f(TAB) f(Q) f(W) f(E) f(R) f(T) f(Y) f(U) f(I) f(O) f(P) f(LEFTBRACKET) f(RIGHTBRACKET) f(BACKSLASH) \
+f(CAPSLOCK) f(A) f(S) f(D) f(F) f(G) f(H) f(J) f(K) f(L) f(SEMICOLON) f(APOSTROPHE) f(RETURN) \
+f(LSHIFT) f(Z) f(X) f(C) f(V) f(B) f(N) f(M) f(COMMA) f(PERIOD) f(SLASH) f(RSHIFT) \
+f(LCTRL) f(APPLICATION) f(LALT) f(SPACE) f(RALT) f(RCTRL) \
+f(UP) f(DOWN) f(LEFT) f(RIGHT) f(INSERT) f(DELETE) f(HOME) f(END) f(PAGEUP) f(PAGEDOWN)
+
+#define _KEY_NAME(k) _KEY_##k,
+enum {
+  _KEY_NONE = 0,
+  MAP(_KEYS, _KEY_NAME)
+};
+
+
+
+//timer
+void timer_init();
+uint32_t timer_read(uint32_t addr);
+
+//uart
+void uart_write(uint32_t data);
+
+
+#endif
