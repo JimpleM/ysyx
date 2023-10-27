6d7962863c6eb5f4b2abda7a59ecbbd6d910de4e
tracer-ysyx <tracer@ysyx.org>
2023-10-27 19:51:14 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:51:14 up 10:37,  1 user,  load average: 0.49, 0.18, 0.15

--- npc/sim_src/device/device_kb.cpp
+++ npc/sim_src/device/device_kb.cpp
@@ -2,22 +2,22 @@
 
 
 
-// void keyboard_update(){
-//     SDL_Event event;
-//     while (SDL_PollEvent(&event)) {
-//     switch (event.type) {
-//       case SDL_QUIT:
-//         nemu_state.state = NEMU_QUIT;
-//         break;
-//       // If a key was pressed
-//       case SDL_KEYDOWN:
-//       case SDL_KEYUP: {
-//         uint8_t k = event.key.keysym.scancode;
-//         bool is_keydown = (event.key.type == SDL_KEYDOWN);
-//         send_key(k, is_keydown);
-//         break;
-//       }
-//       default: break;
-//     }
-//   }
-// }
+void keyboard_update(){
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        nemu_state.state = NEMU_QUIT;
+        break;
+      // If a key was pressed
+      case SDL_KEYDOWN:
+      case SDL_KEYUP: {
+        uint8_t k = event.key.keysym.scancode;
+        bool is_keydown = (event.key.type == SDL_KEYDOWN);
+        send_key(k, is_keydown);
+        break;
+      }
+      default: break;
+    }
+  }
+}
