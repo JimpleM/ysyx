893c686ee954e909ef583174ff10e030fcbc1fda
tracer-ysyx <tracer@ysyx.org>
2023-10-29 16:40:00 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:40:00 up  3:04,  1 user,  load average: 1.11, 1.21, 1.09

--- am-kernels/tests/am-tests/src/tests/keyboard.c
+++ am-kernels/tests/am-tests/src/tests/keyboard.c
@@ -27,8 +27,8 @@ static void drain_keys() {
 
 void keyboard_test() {
   printf("Try to press any key (uart or keyboard)...\n");
+   printf("Try to press any key (uart or keyboard)...\n");
   has_uart = io_read(AM_UART_CONFIG).present;
-    printf("darinkey\n");
   has_kbd  = io_read(AM_INPUT_CONFIG).present;
   while (1) {
     drain_keys();
