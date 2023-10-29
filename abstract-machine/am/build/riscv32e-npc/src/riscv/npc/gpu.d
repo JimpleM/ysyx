7e54c7437d706cdf80eb7d3886deeb1846bab4fa
tracer-ysyx <tracer@ysyx.org>
2023-10-29 22:41:39 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:39 up  9:06,  1 user,  load average: 0.23, 0.46, 0.55

---
+++ abstract-machine/am/build/riscv32e-npc/src/riscv/npc/gpu.d
@@ -0,0 +1,7 @@
+/home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/build/riscv32e-npc/src/riscv/npc/gpu.o: \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/src/riscv/npc/gpu.c \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/src/riscv/npc/npc.h \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/src/riscv/riscv.h
