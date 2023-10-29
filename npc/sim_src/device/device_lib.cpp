69033f97fb21ef8d477245eb0baf4feda6f67e6f
tracer-ysyx <tracer@ysyx.org>
2023-10-29 23:41:51 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:41:51 up 52 min,  1 user,  load average: 1.23, 0.91, 0.90

--- npc/sim_src/device/device_lib.cpp
+++ npc/sim_src/device/device_lib.cpp
@@ -15,7 +15,7 @@ uint32_t device_read(uint32_t addr){
     if(addr == RTC_ADDR || addr == RTC_ADDR + 4){
         // printf("timer\n");
         return timer_read(addr);
-    }else if(addr == KBD_ADDR && flag == 1){
+    }else if(addr == KBD_ADDR && flag == 1 && top->clk == 1){
         return keyboard_read();
     }
     flag = 1;
