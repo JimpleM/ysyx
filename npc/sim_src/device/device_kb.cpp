879f2f0d8e58849e8b100ef86890e0e5c3d8074b
tracer-ysyx <tracer@ysyx.org>
2023-10-29 23:55:57 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:55:57 up  1:06,  1 user,  load average: 0.36, 0.82, 0.93

--- npc/sim_src/device/device_kb.cpp
+++ npc/sim_src/device/device_kb.cpp
@@ -57,7 +57,7 @@ void keyboard_update();
 
 int keyboard_update_adapter(void* data) {
     while(1){
-        SDL_Delay(100);
+        SDL_Delay(10);
         keyboard_update();
     }
     return 0;  // 返回一个整数值
