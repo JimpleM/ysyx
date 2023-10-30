a4455ff1b11836944605ab91a44de801c910f5f6
tracer-ysyx <tracer@ysyx.org>
2023-10-30 14:57:08 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:57:08 up  4:42,  2 users,  load average: 1.56, 1.16, 1.00

--- npc/sim_src/device/device_kb.cpp
+++ npc/sim_src/device/device_kb.cpp
@@ -90,6 +90,8 @@ void keyboard_update(){
         uint8_t k = event.key.keysym.scancode;
         bool is_keydown = (event.key.type == SDL_KEYDOWN) || (event.key.type == SDL_KEYUP);
         send_key(k, is_keydown);
+        send_key(k, is_keydown);
+        send_key(k, is_keydown);
         printf("push num:%d\n",(key_tail+KEY_QUEUE_LEN-key_head)%KEY_QUEUE_LEN);
         break;
       }
