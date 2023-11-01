a52551dcfe5867197506beb095ea1c175b149591
tracer-ysyx <tracer@ysyx.org>
2023-11-01 17:12:41 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:41 up  5:14,  2 users,  load average: 0.49, 0.43, 0.27

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -62,13 +62,13 @@ void init_ftrace(const char *elf_file){
 	// Read symtab
 	int number = sym_hdr.sh_size/sizeof(Elf32_Sym);
 	ReadDataFromFile(&buffer, sizeof(Elf32_Sym), number, sym_hdr.sh_offset, elf_fp);
-	const Elf32_Sym* pSym = (const Elf32_Sym *) buffer;
-	for(int i=0; i<number; i++){
-		if((pSym[i].st_info & 0x0f) == STT_FUNC){
-			// printf("%x %d\n",pSym[i].st_value,pSym[i].st_name);
-			// memcpy(&func_trace[func_cnt++].symbol, &pSym[i], sizeof(Elf32_Sym));
-		}
-	}
+	// const Elf32_Sym* pSym = (const Elf32_Sym *) buffer;
+	// for(int i=0; i<number; i++){
+	// 	if((pSym[i].st_info & 0x0f) == STT_FUNC){
+	// 		// printf("%x %d\n",pSym[i].st_value,pSym[i].st_name);
+	// 		memcpy(&func_trace[func_cnt++].symbol, &pSym[i], sizeof(Elf32_Sym));
+	// 	}
+	// }
 	Assert(func_cnt < FUNC_NUM, "The number of symbol is out of range, please increse the SYM_NUM\n");
 
 	// for(int i=0; i<sym_cnt; i++){
