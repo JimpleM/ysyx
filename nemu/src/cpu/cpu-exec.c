17104bb7bb3aae3b3c93965dd0c96a0a9fa7878e
tracer-ysyx <tracer@ysyx.org>
2023-11-01 16:58:07 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:07 up  4:59,  2 users,  load average: 0.06, 0.15, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -38,9 +38,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-  // if(_this->pc >= 0x80001400){
-  //   puts(_this->logbuf);
-  // }
+  if(_this->pc >= 0x80001400){
+    puts(_this->logbuf);
+  }
   
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
