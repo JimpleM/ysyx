d890969228ac27ecf993d80c52501ce6f829a18c
tracer-ysyx <tracer@ysyx.org>
2023-11-05 19:40:46 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:40:46 up 46 min,  1 user,  load average: 1.74, 1.55, 1.23

--- npc/sim_src/sdb/ftrace.cpp
+++ npc/sim_src/sdb/ftrace.cpp
@@ -27,7 +27,7 @@ void init_ftrace(const char *elf_file){
 	elf_fp = stdout;
 	if (elf_fp != NULL) {
 		FILE *fp = fopen(elf_file, "r");
-		Assert(fp, "Can not open '%s'", elf_file);
+		Assert(fp, "Can not open elf_file '%s'", elf_file);
 		elf_fp = fp;
 	}
 	Log("ELF is read from %s", elf_file ? elf_file : "stdout");
