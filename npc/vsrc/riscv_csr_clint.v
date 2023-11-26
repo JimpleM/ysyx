ad641f1dd7c88e771967ecb5d254126602d6f4e8
tracer-ysyx <tracer@ysyx.org>
2023-11-26 18:35:18 +0800

    > sim RTL ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:35:18 up  6:25,  1 user,  load average: 1.18, 0.50, 0.28

--- npc/vsrc/riscv_csr_clint.v
+++ npc/vsrc/riscv_csr_clint.v
@@ -8,11 +8,34 @@ module riscv_csr_regfile (
 
     input       [`CSR_OPT_WIDTH-1:0]    csr_opt,
 
+    output 
+
 );
 
 always(*)begin
     case(csr_opt)
-        
+        `CSR_OPT_EINST : begin 
+            case(imm[11:0])
+                12'b0000_0000_0000:begin // ecall
+
+                end
+                12'b0011_0000_0010:begin //mret
+
+                end
+                12'b0000_0000_0001:begin //ebreak
+
+                end 
+            endcase
+        end
+        `CSR_OPT_CSRRW : begin 
+
+        end
+        `CSR_OPT_CSRRS : begin 
+
+        end
+        `CSR_OPT_CSRRC : begin 
+
+        end
     endcase
 end
 
