e70f6e776fcb422c0cd056bef48d5d4c2682b3c6
tracer-ysyx <tracer@ysyx.org>
2023-11-26 19:57:59 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:59 up  7:48,  1 user,  load average: 0.78, 0.34, 0.23

--- npc/vsrc/riscv_csr_clint.v
+++ npc/vsrc/riscv_csr_clint.v
@@ -10,6 +10,7 @@ module riscv_csr_clint (
 
     input       [`CSR_OPT_WIDTH-1:0]    csr_opt,
 
+    output      [`DATA_WIDTH-1:0]       csr_rddata,
     output      [`DATA_WIDTH-1:0]       csr_pc
 
 );
@@ -34,6 +35,16 @@ wire    [`DATA_WIDTH-1:0]       csr_read_data           ;
 assign csr_pc = csr_read_data;
 
 always@(*)begin
+    csr_mstatus_write_data  = 32'd0;
+    csr_mcause_write_data   = 32'd0;
+    csr_mepc_write_data     = 32'd0;
+    csr_read_addr           = 32'd0;
+    csr_write_addr          = 0;
+    csr_write_data          = 0;
+    csr_write_valid         = 1'b0;
+    csr_mstatus_write_valid = 1'b0;
+    csr_mepc_write_valid    = 1'b0;
+    csr_mcause_write_valid  = 1'b0;
     case(csr_opt)
         `CSR_OPT_EINST : begin 
             case(imm[11:0])
@@ -42,24 +53,40 @@ always@(*)begin
                     csr_mcause_write_data   = 32'd11;
                     csr_mepc_write_data     = pc;
                     csr_read_addr           = {20'd0,`CSR_MTVEC};
+                    csr_mstatus_write_valid = 1'b1;
+                    csr_mepc_write_valid    = 1'b1;
+                    csr_mcause_write_valid  = 1'b1; 
                 end
                 12'b0011_0000_0010:begin //mret
                     csr_read_addr           = {20'd0,`CSR_MEPC};
                     csr_mstatus_write_data  = csr_mstatus_read_data & ~(32'h0000_1800);
+                    csr_mstatus_write_valid = 1'b1;
                 end
                 12'b0000_0000_0001:begin //ebreak
-
+                    
                 end 
             endcase
         end
         `CSR_OPT_CSRRW : begin 
-
+            csr_read_addr   = imm[11:0];
+            csr_rddata      = csr_read_data;
+            csr_write_addr  = imm[11:0];
+            csr_write_data  = rs1_data;
+            csr_write_valid = 1'b1;
         end
         `CSR_OPT_CSRRS : begin 
-
+            csr_read_addr   = imm[11:0];
+            csr_rddata      = csr_read_data;
+            csr_write_addr  = imm[11:0];
+            csr_write_data  = rs1_data;
+            csr_write_valid = 1'b1;
         end
         `CSR_OPT_CSRRC : begin 
-
+            csr_read_addr   = imm[11:0];
+            csr_rddata      = csr_read_data;
+            csr_write_addr  = imm[11:0];
+            csr_write_data  = rs1_data;
+            csr_write_valid = 1'b1;
         end
     endcase
 end
