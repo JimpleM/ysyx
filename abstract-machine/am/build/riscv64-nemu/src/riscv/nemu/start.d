e66a0b6e8a6fcee396e5ceb0f7a23360758fe138
tracer-ysyx <tracer@ysyx.org>
2023-11-30 18:41:50 +0800

    > compile NEMU ysyx_22040000 马敬鹏 Linux jimple-ubuntu 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:41:50 up  8:26,  1 user,  load average: 0.15, 0.51, 0.38

---
+++ abstract-machine/am/build/riscv64-nemu/src/riscv/nemu/start.d
@@ -0,0 +1,2 @@
+/home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/build/riscv64-nemu/src/riscv/nemu/start.o: \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/src/riscv/nemu/start.S
---
+++ abstract-machine/am/build/riscv64-nemu/src/riscv/nemu/trap.d
@@ -0,0 +1,2 @@
+/home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/build/riscv64-nemu/src/riscv/nemu/trap.o: \
+ /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/src/riscv/nemu/trap.S
