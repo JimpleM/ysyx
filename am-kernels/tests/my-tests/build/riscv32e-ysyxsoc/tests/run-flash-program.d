b64eeeee0110f14d480d585a6006d990a73269e2
jimple_ubuntu <jimple_ubuntu>
2024-05-27 18:51:02 +0800

    finishedrun program from flash

---
+++ am-kernels/tests/my-tests/build/riscv32e-ysyxsoc/tests/run-flash-program.d
@@ -0,0 +1,2 @@
+/home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/tests/my-tests/build/riscv32e-ysyxsoc/tests/run-flash-program.o: \
+ /home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/tests/my-tests/tests/run-flash-program.c
