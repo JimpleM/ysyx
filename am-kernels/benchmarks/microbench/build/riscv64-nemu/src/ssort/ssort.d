8cd9c55fa813e8d9e5117381231ea63f4200d401
jimple_ubuntu <jimple_ubuntu>
2024-05-27 18:16:47 +0800

    finished read from flash by XIP

--- am-kernels/benchmarks/microbench/build/riscv64-nemu/src/ssort/ssort.d
+++
@@ -1,8 +0,0 @@
-/home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/benchmarks/microbench/build/riscv64-nemu/src/ssort/ssort.o: \
- /home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/benchmarks/microbench/src/ssort/ssort.cc \
- /home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/benchmarks/microbench/include/benchmark.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/include/klib.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
--- am-kernels/tests/cpu-tests/build/riscv32e-ysyxsoc/tests/crc32.d
+++
@@ -1,8 +0,0 @@
-/home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32e-ysyxsoc/tests/crc32.o: \
- /home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/tests/crc32.c \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/include/klib-macros.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/am-kernels/tests/cpu-tests/include/trap.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/include/klib.h
