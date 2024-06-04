8fd022421f9300db6b45b6a4384bbae9d010bbc4
jimple_ubuntu <jimple_ubuntu>
2024-06-04 11:53:13 +0800

    finished sdram word extend

--- am-kernels/tests/my-tests/tests/sdram-test.c
+++ am-kernels/tests/my-tests/tests/sdram-test.c
@@ -8,7 +8,7 @@ static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)a
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
 
-#define SIZE 1024
+#define SIZE 1024*1024
 int main(){
 
     // outl(0xa6000000,0x010203F4);
@@ -33,37 +33,39 @@ int main(){
     // data = inl(0xa0000004);
     // printf("%08x\n",data);
     uint32_t i=0;
-    uint8_t data8;    
-    for(i=0; i<SIZE; i++){
-        outb(0xa0000000+i,i&0x000000FF);
-        outb(0xa4000000+i,(0xFFFFFFFF-i)&0x000000FF);
-    }
-    for(i=0; i<SIZE; i++){
-        data8 = inb(0xa0000000+i);
-        check(data8 == (i&0x000000FF));
-        data8 = inb(0xa4000000+i);
-        check(data8 == ((0xFFFFFFFF-i)&0x000000FF));
-    }
+    // uint8_t data8;    
+    // for(i=0; i<SIZE; i++){
+    //     outb(0xa0000000+i,i&0x000000FF);
+    //     outb(0xa4000000+i,(0xFFFFFFFF-i)&0x000000FF);
+    // }
+    // for(i=0; i<SIZE; i++){
+    //     data8 = inb(0xa0000000+i);
+    //     check(data8 == (i&0x000000FF));
+    //     data8 = inb(0xa4000000+i);
+    //     check(data8 == ((0xFFFFFFFF-i)&0x000000FF));
+    // }
 //     uint16_t data16;
 //     for(i = 0; i< SIZE; i+=2){
 //         outw(0xa0000000+i,i&0x0000FFFF);
 //     }
-//     uint32_t data32;
-//     for(i = 0; i< SIZE; i+=4){
-//         outl(0xa0000000+i,i<<8);
-//     }
-//     for(i=0; i<SIZE; i+=2){
-//         data16 = inw(0xa0001000+i*0x10+0x08);
-//         if(data16 != (i&0x0000FFFF) ){
-//             printf("addr:%8x,rdata:%8x,data:%8x\n",0xa0001000+i*0x10+0x08,data16,(i&0x0000FFFF));
-//         }
-//         check(data16 == (i&0x0000FFFF));
-//     }
-//     for(i=0; i<SIZE; i+=4){
-//         data32 = inl(0xa0001000+i*0x10);
-//         if(data32 != (i<<8) ){
-//             printf("addr:%8x,rdata:%8x,data:%8x\n",0xa0001000+i*0x10,data32,(i<<8));
-//         }
-//         check(data32 == (i<<8));
-//     }
+    uint32_t data32;
+    for(i = 0; i< SIZE; i+=4){
+        outl(0xa0000000+i,i<<8);
+        outl(0xa4000000+i,(0xFFFFFFFF-i));
+    }
+    for(i=0; i<SIZE; i+=4){
+        data32 = inl(0xa0000000+i);
+        if(data32 != (i<<8) ){
+            printf("addr:%8x,rdata:%8x,data:%8x\n",0xa0000000+i,data32,(i<<8));
+        }
+        check(data32 == (i<<8));
+        
+    }
+    for(i=0; i<SIZE; i+=4){
+        data32 = inl(0xa4000000+i);
+        if(data32 != (0xFFFFFFFF-i)){
+            printf("addr:%8x,rdata:%8x,data:%8x\n",0xa4000000+i,data32,(0xFFFFFFFF-i));
+        }
+        check(data32 == (0xFFFFFFFF-i));
+    }
 }
