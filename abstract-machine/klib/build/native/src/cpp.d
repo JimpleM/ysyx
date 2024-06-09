a8577f25bf1644f69582f5aa381581053ae3e09e
jimple_ubuntu <jimple_ubuntu>
2024-06-09 19:39:10 +0800

    find the bug of rtt of rtconfig.h RT_MAIN_THREAD_STACK_SIZE is 2048

--- abstract-machine/klib/build/native/src/cpp.d
+++
@@ -1,6 +0,0 @@
-/home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/build/native/src/cpp.o: \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/src/cpp.c \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/arch/native.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/include/klib.h
--- abstract-machine/klib/build/riscv32e-nemu/src/int64.d
+++
@@ -1,5 +0,0 @@
-/home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/build/riscv32e-nemu/src/int64.o: \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/klib/src/int64.c \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/jimple/Documents/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h
